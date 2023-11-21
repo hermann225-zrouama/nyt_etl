ceaece534f830a6899fca55f73870b7842975746
hermann225-zrouama <62513048+hermann225-zrouama@users.noreply.github.com>
2023-11-21 09:06:41 +0000

    Update fetch_data.py
    remove API key

--- dags/scripts/fetch_data.py
+++ dags/scripts/fetch_data.py
@@ -5,7 +5,7 @@ import time
 
 # get data from api
 
-def get_data(begin_date, end_date, sort="newest", query="finance", api_key="wcVWQda87VRaAawNlhBhNsXkpX4XN9It"):
+def get_data(begin_date, end_date, sort="newest", query="finance", api_key=""):
     url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
     params = {
         'api-key': api_key,
